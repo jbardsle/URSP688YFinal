d59f397a8d2f275614695182ccc8a5999e103696
jbardsle <158089582+jbardsle@users.noreply.github.com>
2024-05-13 22:31:05 -0400

    Create CleanPcts.py

---
+++ CleanPcts.py
@@ -0,0 +1,7 @@
+#a function to ensure that there are no nulls and that percents are in
+#decimal format
+def CleanPcts(input):
+  input['Hisp_Pct2'] = None
+  input.loc[(input['Hisp_Pct'] == 0) | (input['Hisp_Pct'].isnull()), 'Hisp_Pct2'] = 0
+  input.loc[input['Hisp_Pct'] > 0, 'Hisp_Pct2'] = input['Hisp_Pct'] / 100
+  return input
