d418596b8723ea7654fc72b54e1958e49aef74ce
jbardsle <158089582+jbardsle@users.noreply.github.com>
2024-05-13 22:35:56 -0400

    Update and rename CleanPcts.py to clean_pcts.py

--- src/modules/clean_pcts.py
+++ src/modules/clean_pcts.py
@@ -1,6 +1,6 @@
 #a function to ensure that there are no nulls and that percents are in
 #decimal format
-def CleanPcts(input):
+def clean_pcts(input):
   input['Hisp_Pct2'] = None
   input.loc[(input['Hisp_Pct'] == 0) | (input['Hisp_Pct'].isnull()), 'Hisp_Pct2'] = 0
   input.loc[input['Hisp_Pct'] > 0, 'Hisp_Pct2'] = input['Hisp_Pct'] / 100
